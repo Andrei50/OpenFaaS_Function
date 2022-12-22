31e989cbafe127bb346e767310a56e30d4c00683
Andrei50 <51993617+Andrei50@users.noreply.github.com>
2022-12-23 01:34:47 +0300

    Update function.sh

--- function.sh
+++ function.sh
@@ -6,4 +6,6 @@ sudo chmod 666 /var/run/docker.sock
 docker login
 faas-cli login -u admin -p lHzs44tlIlX8srvhbu3eR5r3uTstXGFAawnpdZQhl0S8cMs8sSpMcqTUjjf6aqw -g http://10.20.14.147:8080 #password from first VM
 faas-cli store deploy Figlet #install from store for your function
+git clone https://github.com/Andrei50/Figlet_OpenFaaS 
+cd Figlet_OpenFaaS
 faas-cli up -f ./show-html.yml
