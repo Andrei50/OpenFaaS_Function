779ce56d02dbd953c4fde89646dc3e0bcf6bd046
Andrei50 <51993617+Andrei50@users.noreply.github.com>
2022-12-23 01:23:07 +0300

    Update function.sh

--- function.sh
+++ function.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 curl -sSL https://cli.openfaas.com | sudo sh #intall faas-cli
 export OPENFAAS_URL=https://10.20.14.147:8080 #your IP from faasd (from first VM)
-faas-cli login -u admin -p lHzs44tlIlX8srvhbu3eR5r3uTstXGFAawnpdZQhl0S8cMs8sSpMcqTUjjf6aqw #password from first VM
+faas-cli login -u admin -p lHzs44tlIlX8srvhbu3eR5r3uTstXGFAawnpdZQhl0S8cMs8sSpMcqTUjjf6aqw -g https://10.20.14.147:8080 #password from first VM
 git clone https://github.com/Andrei50/Figlet_OpenFaaS 
 cd Figlet_OpenFaaS
 sudo chmod 666 /var/run/docker.sock
