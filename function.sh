8a617abdf8a5ee8ea85ca7074c90644982cb355b
Andrei50 <51993617+Andrei50@users.noreply.github.com>
2022-12-23 01:49:48 +0300

    Update function.sh

--- function.sh
+++ function.sh
@@ -4,7 +4,7 @@ export OPENFAAS_URL=http://10.20.14.147:8080 #your IP from faasd (from first VM)
 cd Figlet_OpenFaaS
 sudo chmod 666 /var/run/docker.sock
 docker login
-faas-cli login -u admin -p lHzs44tlIlX8srvhbu3eR5r3uTstXGFAawnpdZQhl0S8cMs8sSpMcqTUjjf6aqw -g http://10.20.14.147:8080 #password from first VM
+faas-cli login -u admin -p lHzs44tlIlX8srvhbu3eR5r3uTstXGFAawnpdZQhl0S8cMs8sSpMcqTUjjf6aqw -g http://10.20.14.147:8080 #password and gateway from first VM
 faas-cli store deploy Figlet #install from store for your function
 git clone https://github.com/Andrei50/Figlet_OpenFaaS 
 cd Figlet_OpenFaaS
